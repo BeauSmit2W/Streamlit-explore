400a1a13efabfd1d5bd0758f0bc51245f36ec555
Beau Smit <bsmit@2ndwatch.com>
2023-03-07 18:03:51 -0600

    limit output

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from FOOD_INSPECTIONS;")
+rows = run_query("SELECT * from FOOD_INSPECTIONS limit 25;")
 
 # Print results.
 for row in rows:
