4aa0f5fa2a64787e6bd1e7f07d8269ec881db7fd
Beau Smit <bsmit@2ndwatch.com>
2023-03-07 18:57:54 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 import folium
+from streamlit_folium import st_folium
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
@@ -30,4 +31,13 @@ df.LATITUDE = df.LATITUDE.astype(float)
 
 # Print results.
 st.dataframe(df)
-st.write(f"data type: {df.dtypes}")
+[st.write(t) for t in df.dtypes]
+
+# map of Chicago
+m = folium.Map(location=[41.88148170412358, -87.63162352073498], zoom_start=16)
+folium.Marker(
+    [41.87768968860344, -87.63705780095162], popup="2nd Watch Office", tooltip="2nd Watch Office"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
