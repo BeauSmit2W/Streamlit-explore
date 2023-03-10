d4719e11721ed4407431d087a991e9a5827471ab
Beau Smit <bsmit@2ndwatch.com>
2023-03-10 13:16:39 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import snowflake.connector
 import pandas as pd
+import numpy as np
 import folium
 from streamlit_folium import st_folium
 
@@ -28,25 +29,45 @@ df = run_query("SELECT * from FOOD_INSPECTIONS where RESULTS='Fail' limit 50;")
 
 st.write(f"dataframe shape: {df.shape}")
 
-df.LONGITUDE = df.LONGITUDE.astype(float)
-df.LATITUDE = df.LATITUDE.astype(float)
+# Randomly fill a dataframe and cache it
+@st.cache(allow_output_mutation=True)
+def get_dataframe():
+    return df
 
-# Print results.
+df = get_dataframe()
+
+# Create row, column, and value inputs
+row = st.number_input('row', max_value=df.shape[0])
+col = st.number_input('column', max_value=df.shape[1])
+value = st.number_input('value')
+
+# Change the entry at (row, col) to the given value
+df.values[row][col] = value
+
+# And display the result!
 st.dataframe(df)
 
-# map of Chicago
-m = folium.Map(location=[41.88148170412358, -87.63162352073498], zoom_start=15)
 
-# add markers to map
-folium.Marker(
-    [41.87768968860344, -87.63705780095162], popup="2nd Watch Office", tooltip="2nd Watch Office"
-).add_to(m)
 
-for idx, row in df.iterrows():
-    if pd.notnull(row.LATITUDE) & pd.notnull(row.LONGITUDE):
-        folium.Marker(
-            [row.LATITUDE, row.LONGITUDE], popup=row.DBA_Name, tooltip=row.AKA_Name
-        ).add_to(m)
+# df.LONGITUDE = df.LONGITUDE.astype(float)
+# df.LATITUDE = df.LATITUDE.astype(float)
 
-# call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
+# # Print results.
+# st.dataframe(df)
+
+# # map of Chicago
+# m = folium.Map(location=[41.88148170412358, -87.63162352073498], zoom_start=15)
+
+# # add markers to map
+# folium.Marker(
+#     [41.87768968860344, -87.63705780095162], popup="2nd Watch Office", tooltip="2nd Watch Office"
+# ).add_to(m)
+
+# for idx, row in df.iterrows():
+#     if pd.notnull(row.LATITUDE) & pd.notnull(row.LONGITUDE):
+#         folium.Marker(
+#             [row.LATITUDE, row.LONGITUDE], popup=row.DBA_Name, tooltip=row.AKA_Name
+#         ).add_to(m)
+
+# # call to render Folium map in Streamlit
+# st_data = st_folium(m, width=725)
